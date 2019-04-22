9e091a5b9981a21f4ae9178a87e7ee3dacb85d17
Angel <angel.angel802@gmail.com>
2019-04-22 17:26:09 +0200

    Class GPU done

--- PCBuilder/src/componentsInfo/GPU.java
+++ PCBuilder/src/componentsInfo/GPU.java
@@ -12,11 +12,47 @@ package componentsInfo;
 public class GPU extends PcComponent{
     
     private double height;
-    private double weight;
     private double longitude;
     private int recommendedPSU;
     
     public GPU() {
         super();
+        
+        this.setHeight(0);
+        this.setLongitude(0);
+        this.setRecommendedPSU(0);
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getRecommendedPSU() {
+        return recommendedPSU;
+    }
+    private double weight;
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setRecommendedPSU(int recommendedPSU) {
+        this.recommendedPSU = recommendedPSU;
     }
 }
