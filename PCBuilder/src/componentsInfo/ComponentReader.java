5ab9584b0b97843575dec1681c1ab509323faea2
TheBender <i62gomos@uco.es>
2019-04-23 17:14:38 +0200

    Added reading functions for PcBox, Motherboard and CPU classes

--- PCBuilder/src/componentsInfo/ComponentReader.java
+++ PCBuilder/src/componentsInfo/ComponentReader.java
@@ -6,11 +6,11 @@
 package componentsInfo;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Vector;
 
 /**
  *
@@ -18,10 +18,92 @@ import java.util.logging.Logger;
  */
 public class ComponentReader {
     
-    public static GPU[] readGPUs() throws IOException {
+    public static ArrayList<PcBox> readPcBox() throws FileNotFoundException {
         
-        GPU[] array = new GPU[5];
-        int i = 0;
+        ArrayList<PcBox> array = new ArrayList();
+        
+        File file = new File ("DataBases/PCBoxes.txt");
+        
+        Scanner sc = new Scanner(file).useDelimiter("[$]");
+        
+        while(sc.hasNext()) {
+            StringTokenizer st = new StringTokenizer(sc.next(), ";");
+            PcBox box = new PcBox();
+            
+            box.setName(st.nextToken());
+            box.setPrize(Double.parseDouble(st.nextToken()));
+            
+            //lectura de los factores forma
+            StringTokenizer aux = new StringTokenizer(st.nextToken(), ",");
+            Vector factorForms = new Vector();
+            while(aux.hasMoreTokens()) {
+                factorForms.add(aux.nextToken());
+            }
+            box.setFormFactors(factorForms);
+            
+            box.setHeight(Double.parseDouble(st.nextToken()));
+            box.setWeight(Double.parseDouble(st.nextToken()));
+            box.setLongitude(Double.parseDouble(st.nextToken()));
+            box.setMaxGPU(Double.parseDouble(st.nextToken()));
+            box.setDescription(st.nextToken());
+            
+            array.add(box);
+        }
+        
+        return array;
+    }
+    
+    public static ArrayList<Motherboard> readMotherboards() throws FileNotFoundException {
+        
+        ArrayList<Motherboard> array = new ArrayList();
+        
+        File file = new File ("DataBases/Motherboards.txt");
+        
+        Scanner sc = new Scanner(file).useDelimiter("[$]");
+        
+        while(sc.hasNext()) {
+            StringTokenizer st = new StringTokenizer(sc.next(), ";");
+            Motherboard mb = new Motherboard();
+            
+            mb.setName(st.nextToken());
+            mb.setPrize(Double.parseDouble(st.nextToken()));
+            mb.setFormFactor(st.nextToken());
+            mb.setSocket(st.nextToken());
+            mb.setRamCompatibility(Integer.parseInt(st.nextToken()));
+            mb.setDescription(st.nextToken());
+            
+            array.add(mb);
+        }
+        
+        return array;
+    }
+    
+    public static ArrayList<CPU> readCPUs() throws FileNotFoundException {
+        
+        ArrayList<CPU> array = new ArrayList();
+        
+        File file = new File ("DataBases/CPUs.txt");
+        
+        Scanner sc = new Scanner(file).useDelimiter("[$]");
+        
+        while(sc.hasNext()) {
+            StringTokenizer st = new StringTokenizer(sc.next(), ";");
+            CPU cpu = new CPU();
+            
+            cpu.setName(st.nextToken());
+            cpu.setPrize(Double.parseDouble(st.nextToken()));
+            cpu.setSocket(st.nextToken());
+            cpu.setDescription(st.nextToken());
+            
+            array.add(cpu);
+        }
+        
+        return array;
+    }
+        
+    public static ArrayList<GPU> readGPUs() throws FileNotFoundException {
+        
+        ArrayList<GPU> array = new ArrayList();
         
         File file = new File ("DataBases/GPUS.txt");
         
@@ -39,11 +121,9 @@ public class ComponentReader {
             gpu.setRecommendedPSU(Integer.parseInt(st.nextToken()));
             gpu.setDescription(st.nextToken());
             
-            array[i] = gpu;
-            i++;
+            array.add(gpu);
         }
         
         return array;
     }
-    
 }
