8ace5b62d75640a460fa971bc6b818997e8febf0
TheBender <i62gomos@uco.es>
2019-04-28 12:47:15 +0200

    Setted pannels hierarchy

--- PCBuilder/src/mainPage/mainPage.java
+++ PCBuilder/src/mainPage/mainPage.java
@@ -37,7 +37,7 @@ public class mainPage extends javax.swing.JFrame {
         initComponents();
         
         //Safe the tab selected, necesary later
-        this.tabSelected = jTabbedPane1.getSelectedIndex();
+        this.tabSelected = components.getSelectedIndex();
     }
 
     /**
@@ -49,7 +49,8 @@ public class mainPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTabbedPane1 = new javax.swing.JTabbedPane();
+        container = new javax.swing.JPanel();
+        components = new javax.swing.JTabbedPane();
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -66,15 +67,20 @@ public class mainPage extends javax.swing.JFrame {
         jPanel7 = new javax.swing.JPanel();
         jScrollPane8 = new javax.swing.JScrollPane();
         jPanel8 = new javax.swing.JPanel();
+        information = new javax.swing.JPanel();
+        principal = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTabbedPane1.setMaximumSize(new java.awt.Dimension(535, 700));
-        jTabbedPane1.setMinimumSize(new java.awt.Dimension(535, 700));
-        jTabbedPane1.setPreferredSize(new java.awt.Dimension(535, 700));
-        jTabbedPane1.addChangeListener(new javax.swing.event.ChangeListener() {
+        container.setPreferredSize(new java.awt.Dimension(1337, 906));
+        container.setLayout(new java.awt.BorderLayout());
+
+        components.setMaximumSize(new java.awt.Dimension(535, 700));
+        components.setMinimumSize(new java.awt.Dimension(535, 700));
+        components.setPreferredSize(new java.awt.Dimension(535, 700));
+        components.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                jTabbedPane1StateChanged(evt);
+                componentsStateChanged(evt);
             }
         });
 
@@ -84,64 +90,69 @@ public class mainPage extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jPanel1);
 
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("mainPage/Bundle"); // NOI18N
-        jTabbedPane1.addTab(bundle.getString("TAB1"), jScrollPane1); // NOI18N
+        components.addTab(bundle.getString("TAB1"), jScrollPane1); // NOI18N
 
         jPanel2.setLayout(new java.awt.GridBagLayout());
         jScrollPane2.setViewportView(jPanel2);
 
-        jTabbedPane1.addTab(bundle.getString("TAB2"), jScrollPane2); // NOI18N
+        components.addTab(bundle.getString("TAB2"), jScrollPane2); // NOI18N
 
         jPanel3.setLayout(new java.awt.GridBagLayout());
         jScrollPane3.setViewportView(jPanel3);
 
-        jTabbedPane1.addTab(bundle.getString("TAB3"), jScrollPane3); // NOI18N
+        components.addTab(bundle.getString("TAB3"), jScrollPane3); // NOI18N
 
         jPanel4.setLayout(new java.awt.GridBagLayout());
         jScrollPane4.setViewportView(jPanel4);
 
-        jTabbedPane1.addTab(bundle.getString("TAB4"), jScrollPane4); // NOI18N
+        components.addTab(bundle.getString("TAB4"), jScrollPane4); // NOI18N
 
         jPanel5.setLayout(new java.awt.GridBagLayout());
         jScrollPane5.setViewportView(jPanel5);
 
-        jTabbedPane1.addTab(bundle.getString("TAB5"), jScrollPane5); // NOI18N
+        components.addTab(bundle.getString("TAB5"), jScrollPane5); // NOI18N
 
         jPanel6.setLayout(new java.awt.GridBagLayout());
         jScrollPane6.setViewportView(jPanel6);
 
-        jTabbedPane1.addTab(bundle.getString("TAB6"), jScrollPane6); // NOI18N
+        components.addTab(bundle.getString("TAB6"), jScrollPane6); // NOI18N
 
         jPanel7.setLayout(new java.awt.GridBagLayout());
         jScrollPane7.setViewportView(jPanel7);
 
-        jTabbedPane1.addTab(bundle.getString("TAB7"), jScrollPane7); // NOI18N
+        components.addTab(bundle.getString("TAB7"), jScrollPane7); // NOI18N
 
         jPanel8.setLayout(new java.awt.GridBagLayout());
         jScrollPane8.setViewportView(jPanel8);
 
-        jTabbedPane1.addTab(bundle.getString("TAB8"), jScrollPane8); // NOI18N
+        components.addTab(bundle.getString("TAB8"), jScrollPane8); // NOI18N
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(792, Short.MAX_VALUE)
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+        container.add(components, java.awt.BorderLayout.LINE_END);
+
+        information.setPreferredSize(new java.awt.Dimension(1337, 150));
+
+        javax.swing.GroupLayout informationLayout = new javax.swing.GroupLayout(information);
+        information.setLayout(informationLayout);
+        informationLayout.setHorizontalGroup(
+            informationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1337, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(195, Short.MAX_VALUE)
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+        informationLayout.setVerticalGroup(
+            informationLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 150, Short.MAX_VALUE)
         );
+
+        container.add(information, java.awt.BorderLayout.PAGE_END);
+
+        principal.setLayout(new java.awt.BorderLayout());
+        container.add(principal, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(container, java.awt.BorderLayout.PAGE_START);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTabbedPane1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jTabbedPane1StateChanged
+    private void componentsStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_componentsStateChanged
         //Clear the last selected tab for performance improvemment
         switch(this.tabSelected){
             case 0:
@@ -171,12 +182,12 @@ public class mainPage extends javax.swing.JFrame {
         }
         
         //Safe the new selected tab
-        tabSelected = jTabbedPane1.getSelectedIndex();
+        tabSelected = components.getSelectedIndex();
 
         //Read all the components from the database according with the tab index
         ArrayList componentList = new ArrayList();
         try {
-            switch(jTabbedPane1.getSelectedIndex()){
+            switch(components.getSelectedIndex()){
                 case 0:
                     componentList = ComponentReader.readPcBox();
                     break;
@@ -229,7 +240,7 @@ public class mainPage extends javax.swing.JFrame {
 
             //Set the information read from the database in the component
             pcComponent = new PcComponent.PcComponent();
-            switch(jTabbedPane1.getSelectedIndex()){
+            switch(components.getSelectedIndex()){
                 case 0:
                     PcBox pcBox = (PcBox) componentList.get(i);
 
@@ -269,7 +280,6 @@ public class mainPage extends javax.swing.JFrame {
                     pcComponent.setProductName(cooler.getName());
                     pcComponent.setProductPrize(cooler.getPrize());
                     pcComponent.setProductImage(new ImageIcon("DataBases/Coolers_images/" + cooler.getName() + "_142x141.jpg"));
-                    System.out.print("DataBases/Coolers_images/" + cooler.getName() + "_142x141.jpg\n");
 
                     //Add the GPU to the list
                     jPanel4.add(pcComponent, gridBagConstraints);
@@ -321,7 +331,7 @@ public class mainPage extends javax.swing.JFrame {
                     break;
             }
         }
-    }//GEN-LAST:event_jTabbedPane1StateChanged
+    }//GEN-LAST:event_componentsStateChanged
 
     /**
      * @param args the command line arguments
@@ -361,6 +371,9 @@ public class mainPage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTabbedPane components;
+    private javax.swing.JPanel container;
+    private javax.swing.JPanel information;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -377,6 +390,6 @@ public class mainPage extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane6;
     private javax.swing.JScrollPane jScrollPane7;
     private javax.swing.JScrollPane jScrollPane8;
-    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JPanel principal;
     // End of variables declaration//GEN-END:variables
 }
